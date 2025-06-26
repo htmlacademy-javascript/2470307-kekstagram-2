aba99e9a0b7056284c55ec1ca6aa9866df8d299f
Keks <keks@htmlacademy.ru>
2025-06-26 13:11:05 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/render-thumbnails.js
@@ -0,0 +1,33 @@
+import { showBigPicture } from './render-big-picture.js';
+
+const thumbnailsContainer = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderThumbnail = (thumbnail) => {
+  const thumbnailElement = thumbnailTemplate.cloneNode(true);
+  const imageElement = thumbnailElement.querySelector('.picture__img');
+
+  imageElement.src = thumbnail.url;
+  imageElement.alt = thumbnail.description;
+  thumbnailElement.querySelector('.picture__comments').textContent = thumbnail.comments.length;
+  thumbnailElement.querySelector('.picture__likes').textContent = thumbnail.likes;
+
+  thumbnailElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showBigPicture(thumbnail);
+  });
+
+  return thumbnailElement;
+};
+
+const renderThumbnails = (thumbnails) => {
+  thumbnailsContainer.querySelectorAll('.picture').forEach((element) => element.remove());
+  const thumbnailsFragment = document.createDocumentFragment();
+
+  thumbnails?.forEach((thumbnail) => {
+    thumbnailsFragment.appendChild(renderThumbnail(thumbnail));
+  });
+  thumbnailsContainer.appendChild(thumbnailsFragment);
+};
+
+export { renderThumbnails };
---
+++ 11/js/utils.js
@@ -0,0 +1,24 @@
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const toggleClass = (element, className) => {
+  element.classList.toggle(className);
+};
+
+const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  if (num % 10 === 0 || num % 100 > 4 && num % 100 < 21) {
+    return genitivePlural;
+  }
+  return num % 10 === 1
+    ? nominative
+    : genitiveSingular;
+};
+
+const applyDebounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { isEscapeKey, toggleClass, numDecline, applyDebounce };
